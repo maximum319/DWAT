2caac2cef108ec49d0ecca619183e20244845fa4
maximum319 <206453387+maximum319@users.noreply.github.com>
2025-07-15 14:52:16 +0300

    feat: community

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
 
-require("dotenv").config();
+require("dotenv").config({ path: ".env" });
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const HOLESKY_RPC_URL = process.env.HOLESKY_RPC_URL;
 
 module.exports = {
   solidity: {
@@ -18,8 +19,12 @@ module.exports = {
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
     },
+    holesky: {
+      url: HOLESKY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    }
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
-  },
+  }
 };
